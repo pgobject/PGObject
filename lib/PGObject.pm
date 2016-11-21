f338bc5a30b390677b83841659210c59d1b3ed94
Yves Lavoie <ylavoie@yveslavoie.com>
2016-11-21 11:29:01 -0500

    Update PGObject.pm
    Use beginning and end of string markers

--- lib/PGObject.pm
+++ lib/PGObject.pm
@@ -313,7 +313,7 @@ sub call_procedure {
     if ($args{orderby}){
         $order = join(', ', map {
                                   my $dir = undef;
-                                  if ( s/\s+(ASC|DESC)//i ) {
+                                  if ( s/\A\s+(ASC|DESC)\s*\z//i ) {
                                       $dir = $1;
                                   }
                                   defined $dir ? $dbh->quote_identifier($_)
